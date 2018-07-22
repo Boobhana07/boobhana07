4fec5b9d0633f7084c22c0ad03ad55dcb69c4f3e
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-07-22 11:53:09 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,10 @@
+public class Vowel {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='e';
+		if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'||ch=='u') 
+			System.out.println("vowel");
+		else 
+			System.out.println("consonant");
+		}
+}
