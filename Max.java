33b1eb91c3a7167149c6e3a2e40b1cb6711e2ed4
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-07-31 13:39:32 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,13 @@
+public class Max {
+public static void main(String[] args) {
+	int arr[]=new int[] {4,6,7,8,66};
+		int max=arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]>max) 
+		max=arr[i];
+		}
+		System.out.println(max);
+	
+	}
+
+}
