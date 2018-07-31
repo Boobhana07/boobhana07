58efcecc68ddd6868094fb191fcaaa5fe6335138
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-07-31 15:49:02 +0530

    Create PrimeRange.java

---
+++ PrimeRange.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Max {
+public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    System.out.println("enter the number");
+	    int num=in.nextInt();
+	   int n=in.nextInt();
+	    int flag=0;
+	    for(int i=num;i<=n;i++) {
+	    	for(int j=2;j<i;j++) {
+	    	if(i%j==0) {
+	    	flag=0;
+	      	break;
+	    }
+	    	else {
+	    		flag=1;
+	    }
+	    }
+	    	if(flag==1) {
+	    		System.out.println(i);
+	    	}
+	        }}}
+
