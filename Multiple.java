84a77eacb6598dc6bedbebbd9cffa8b375be944d
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-07-31 17:06:09 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Multiple {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int n1=0;
+	for(int i=1;i<=n;i++) {
+		n1=i*n;
+	
+	System.out.println(n1+" ");
+}
+}
+}
+		
