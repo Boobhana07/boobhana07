187c141fdedb3a2be1df208b1c3086a75f09078c
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-07 16:16:26 +0530

    Create Append.java

---
+++ Append.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Append {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		String s1="";
+		s1=in.nextLine();
+		System.out.println(s1+".");
+}
+}
