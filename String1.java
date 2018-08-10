8a1391f67ce18f53afd3383aed19dea17279dbfe
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-10 16:53:57 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,21 @@
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  String str = "vishal";
+		  String str1="vabcded";
+		  for(int i=0;i<str.length();i++) {
+			  count=0;
+			  for(int j=0;j<str1.length();j++) {
+				  if(str.charAt(i)==str1.charAt(j)) {
+					 System.out.println(str1.charAt(j));
+				
+				  } }
+				 
+			 }
+			 
+		  
+		
+	}
+
+}
