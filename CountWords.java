d76d9ebe094297201dafa293f1e9ce15b950cd6e
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-10 19:14:20 +0530

    Create CountWords.java

---
+++ CountWords.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class CountWords {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str[]= {"jhjuhlku","kjjh","fhgyvi"};
+		int count=0;
+		ArrayList<String>al=new ArrayList<String>();
+		for(int i=0;i<str.length;i++) {
+			al.add(str[i]);
+		}
+		System.out.println(al.size());
+		}
+}
