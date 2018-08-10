23ae3380e0e475afbb224aef97d64ddf9936d8fd
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-10 18:28:51 +0530

    Rename Min.java to Min1.java

---
+++ Min1.java
@@ -0,0 +1,8 @@
+public class Median {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  int arr[]= {5,6,7,8,1};
+	  int len=arr.length/2;
+	  System.out.println(arr[len]);
+			}
+	}
