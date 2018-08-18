fc40bff029d8ff9ca845961bc89528d10e4e98f1
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 16:10:09 +0530

    Create StringTime.java

---
+++ StringTime.java
@@ -0,0 +1,8 @@
+public class StringTime {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="laptop";
+		int n=5;
+		for(int i=0;i<n;i++) {
+			System.out.println(str);
+		}}}
