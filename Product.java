3d2cba72a19e17f053bfd34ea4faaa643fbbbf08
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 17:29:01 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,14 @@
+public class Product {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=31;
+		int b=1;
+		int n=a*b;
+		if(n%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+}
