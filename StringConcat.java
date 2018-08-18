eb6664b60cac1ade5f826f4a5687b6124a6a8849
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 16:17:27 +0530

    Create StringConcat.java

---
+++ StringConcat.java
@@ -0,0 +1,8 @@
+public class StringConcate {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello";
+		String s2="world";
+		String s3=s1+s2;
+		System.out.println(s3);
+}}
