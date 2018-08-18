cd9bde8e0c937820bb4fbb347267af53631b31b3
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 15:16:39 +0530

    Create FindSpace1.java

---
+++ FindSpace1.java
@@ -0,0 +1,19 @@
+public class FindSpace1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1=" jhm klj mjkkm.hhh.ghlku.gy";
+int l=s1.length();
+int count=0;
+for(int i=0;i<l;i++)
+{
+	char c=s1.charAt(i);
+    if(c=='.')
+    {
+   count++;
+    }
+  }
+System.out.println(count);
+	}
+
+}
+
