fd3a27c442c6292d2088a714a83cbbd517c8cab7
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 17:42:02 +0530

    Create NaturalNum.java

---
+++ NaturalNum.java
@@ -0,0 +1,9 @@
+public class NaturalNum {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int n=3;
+		int sum=0;
+		sum=sum+n*(n+1)/2;
+		System.out.println(sum);
+		}
+}
