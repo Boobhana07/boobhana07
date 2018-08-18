840461fee463e3c06bbbb3db4fdcfd1db287c386
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 15:27:50 +0530

    Create SwapNum.java

---
+++ SwapNum.java
@@ -0,0 +1,12 @@
+public class SwappingNum {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=37;
+		int b=73;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+	}}
