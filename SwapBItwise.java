1e1c9c19f9db087fdebcd8cdabb8e694cc649b9f
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 15:36:08 +0530

    Create SwapBItwise.java

---
+++ SwapBItwise.java
@@ -0,0 +1,13 @@
+public class SwapBitwise {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=150;
+int b=160;
+a=a^b;
+b=a^b;
+a=a^b;
+System.out.println(a);
+System.out.println(b);
+	}
+
+}
