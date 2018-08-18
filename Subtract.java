03a9776d9e9a4a7dac0cc0a00dd678fdcffc2d58
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 18:15:06 +0530

    Create Subtract.java

---
+++ Subtract.java
@@ -0,0 +1,13 @@
+public class Subtract {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=5;
+		int c=a-b;
+		if(c%2==0) 
+			System.out.println("even");
+		else 
+			System.out.println("odd");
+}
+
+}
