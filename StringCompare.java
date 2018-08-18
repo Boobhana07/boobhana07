501714c721fec79920609bd8e725c7c3f815c48a
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 16:14:52 +0530

    Create StringCompare.java

---
+++ StringCompare.java
@@ -0,0 +1,14 @@
+public class StringCompare {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello";
+		String s2="world";
+		if(s1.length()>s2.length()) {
+			System.out.println(s1);
+		}else {
+			System.out.println(s2);
+		}
+
+	}
+
+}
