3f955ef29d046403d1fe91a0329c0c5c1174f058
Boobhana07 <41354924+Boobhana07@users.noreply.github.com>
2018-08-18 17:22:52 +0530

    Create SubOrAdd.java

---
+++ SubOrAdd.java
@@ -0,0 +1,11 @@
+public class SubOrAdd {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=325;
+		if(n%2==0) {
+	System.out.println(n);
+}else{
+	System.out.println(n-1); 
+}
+	}
+}
